266a85833ca077efd453665f4512fa063d7c9fc4
yujin9619 <yujin9588@gmail.com>
2021-09-02 16:48:44 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/newproject/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.nepplus.newproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
